da6f1a22e343a0862c5af8f23e8a43ec458d7163
VishalVSV <vishalsvetrivel@gmail.com>
2021-01-11 18:00:04 +0530

    Started documentation
    +Added documentation to the command handler and config class

--- ConfigService/ConfigFile.cs
+++ ConfigService/ConfigFile.cs
@@ -14,20 +14,51 @@ using Newtonsoft.Json.Serialization;
 
 namespace Clubby.ConfigService
 {
+    /// <summary>
+    /// The class that contains all persistent data for the program
+    /// </summary>
     public class ConfigFile
     {
+        /// <summary>
+        /// Path pointing to the file that this instance deals with.
+        /// </summary>
         protected string filePath = "./config.txt";
 
+        /// <summary>
+        /// The bot token to access discord.
+        /// </summary>
         public string DiscordBotToken = "YOUR_BOT_TOKEN";
+        /// <summary>
+        /// The prefix used to call commands over on discord land.
+        /// </summary>
         public string DiscordBotPrefix = "+";
+        /// <summary>
+        /// Named channels used for various purposes inside the bot implementation and over plugins
+        /// </summary>
         public Dictionary<string, ulong> DiscordChannels = new Dictionary<string, ulong>();
+        /// <summary>
+        /// The Permission handler for this instance of the bot.
+        /// </summary>
         public DiscordPermissions DiscordPermissions = new DiscordPermissions();
+        /// <summary>
+        /// The list of all announcements made by the bot.
+        /// </summary>
         public List<(AnnouncementType, ulong)> DiscordAnnouncements = new List<(AnnouncementType, ulong)>();
+        /// <summary>
+        /// A list of all suggestions made through the bot.
+        /// </summary>
         public Dictionary<ulong, Suggestion> DiscordSuggestions = new Dictionary<ulong, Suggestion>();
+        /// <summary>
+        /// The number of suggestions made through the bot
+        /// </summary>
         public int DiscordSuggestionCount = 0;
 
+        
         public List<Council> Councils = new List<Council>();
 
+        /// <summary>
+        /// A closure that calls a function and relays errors to the discord frontend.
+        /// </summary>
         [JsonIgnore]
         public Action<Action, string> Call;
 
@@ -38,12 +69,26 @@ namespace Clubby.ConfigService
         /// </summary>
         public Dictionary<string, object> DynamicData = new Dictionary<string, object>();
 
+        /// <summary>
+        /// The scheduler for the program.
+        /// 
+        /// NOTE: Periodic functions must be cleared before plugin reload to prevent dangling references.
+        /// </summary>
         public Scheduler scheduler = new Scheduler();
 
+        /// <summary>
+        /// The watcher that reloads the config on editing.
+        /// </summary>
         private FileSystemWatcher config_watcher;
 
+        /// <summary>
+        /// The number of times the auto reloader tries to load the config before it gives up.
+        /// </summary>
         public const int CONFIG_RELOAD_ATTEMPTS = 5;
 
+        /// <summary>
+        /// The json serializer settings to be used throught the program.
+        /// </summary>
         public static JsonSerializerSettings settings = new JsonSerializerSettings();
 
         static ConfigFile()
@@ -55,11 +100,20 @@ namespace Clubby.ConfigService
             };
         }
 
+        /// <summary>
+        /// Closure that gets a channel from an id.
+        /// </summary>
         [JsonIgnore]
         public Func<ulong, SocketTextChannel> GetChannel;
 
+        /// <summary>
+        /// The interval in which the config file saves itself.
+        /// </summary>
         public TimeSpan AutoSaveInterval = TimeSpan.FromMinutes(1);
 
+        /// <summary>
+        /// Save the config instance
+        /// </summary>
         public void Save()
         {
             config_watcher.EnableRaisingEvents = false;
@@ -67,12 +121,20 @@ namespace Clubby.ConfigService
             config_watcher.EnableRaisingEvents = true;
         }
 
+        /// <summary>
+        /// Saves the config file and disables the watcher.
+        /// </summary>
         public void Destroy()
         {
             config_watcher.EnableRaisingEvents = false;
             File.WriteAllText(filePath, JsonConvert.SerializeObject(this, Formatting.Indented,settings));
         }
 
+        /// <summary>
+        /// Returns a default config instance.
+        /// </summary>
+        /// <param name="filePath">The file name to assign to the default instance</param>
+        /// <returns>A config instance with default values</returns>
         public static ConfigFile MakeDefault(string filePath)
         {
             ConfigFile file = new ConfigFile();
@@ -82,6 +144,8 @@ namespace Clubby.ConfigService
             return file;
         }
 
+        // Loads a config file from a path implicitly.
+        // It is probably bad practise to overload a implicit operator for a constructor but I like it so its staying.
         public static implicit operator ConfigFile(string path)
         {
             ConfigFile config = MakeDefault(path);
@@ -100,6 +164,9 @@ namespace Clubby.ConfigService
                 return null;
         }
 
+        /// <summary>
+        /// Callback to reload the config on edit
+        /// </summary>
         private void Config_file_edited(object sender, FileSystemEventArgs e)
         {
             for (int i = 0; i < CONFIG_RELOAD_ATTEMPTS; i++)
@@ -122,6 +189,8 @@ namespace Clubby.ConfigService
                     continue;
                 }
             }
+
+            Logger.Log(this, "Config failed to reload! Save the file again if changes are important!");
         }
     }
 }
--- Discord/CommandHandling/CommandHandler.cs
+++ Discord/CommandHandling/CommandHandler.cs
@@ -10,34 +10,61 @@ using System.Threading.Tasks;
 
 namespace Clubby.Discord.CommandHandling
 {
+    /// <summary>
+    /// Handles all Discord Command Dispatching.
+    /// </summary>
     public class CommandHandler
     {
+        /// <summary>
+        /// Random object used to fuel the bot's sentience
+        /// </summary>
         private static Random rand = new Random();
 
+        /// <summary>
+        /// The Manager for all the Commands loaded from external plugins.
+        /// </summary>
         public PluginManager<IDiscordCommand,DiscordBot> commands = new PluginManager<IDiscordCommand,DiscordBot>();
 
+        /// <summary>
+        /// Redundant closure to get channel for convenience.
+        /// </summary>
         public Func<ulong, SocketTextChannel> GetChannel;
 
+        /// <summary>
+        /// Per user backing for currently executing command. Used to let commands take multi-message input
+        /// </summary>
         [JsonIgnore]
         public Dictionary<ulong, IDiscordCommand> ExecutingCommand = new Dictionary<ulong, IDiscordCommand>();
 
+        /// <summary>
+        /// Private reference to the bot that owns the handler
+        /// </summary>
         [JsonIgnore]
         private DiscordBot bot;
 
         public CommandHandler(DiscordBot bot)
         {
             this.bot = bot;
+            // Check and load command plugins if found.
             if (Directory.Exists("./Commands"))
             {
                 commands.Load("./Commands", bot);
             }
         }
 
+        /// <summary>
+        /// Reload the plugins from the commands directory.
+        /// </summary>
         public void Reload()
         {
             commands.Load("./Commands",bot);
         }
 
+        /// <summary>
+        /// Gets all commands that the user can use.
+        /// </summary>
+        /// <param name="max_perms">The maximum permission that the user can access</param>
+        /// <returns>Types of the commands that the user can access</returns>
         public Type[] GetAllCommands(int max_perms)
         {
             return commands.GetPlugins((f) =>
@@ -46,6 +73,10 @@ namespace Clubby.Discord.CommandHandling
             }).ToArray();
         }
 
+        /// <summary>
+        /// Get the command that the user is executing.
+        /// </summary>
+        /// <param name="id">The id of the user to check</param>
         public IDiscordCommand GetExecutingCommand(ulong id)
         {
             if (ExecutingCommand.ContainsKey(id))
@@ -53,6 +84,11 @@ namespace Clubby.Discord.CommandHandling
             else return null;
         }
 
+        /// <summary>
+        /// Set the executing command of a user.
+        /// </summary>
+        /// <param name="id">The id of the user</param>
+        /// <param name="cmd">The command to set as executing</param>
         public void SetExecutingCommand(ulong id, IDiscordCommand cmd)
         {
             if (ExecutingCommand.ContainsKey(id))
@@ -61,62 +97,91 @@ namespace Clubby.Discord.CommandHandling
                 ExecutingCommand.Add(id, cmd);
         }
 
-
+        /// <summary>
+        /// Handles a Discord Message and manages dynamic dispatch.
+        /// </summary>
+        /// <param name="msg">The Discord text message</param>
+        /// <param name="current_user">The id of the user that sent the message</param>
+        /// <returns></returns>
         public async Task Handle(SocketMessage msg, ulong current_user)
         {
+            // Check if the msg mentions the bot.
+            // TODO: Add conditional replies
             if (msg.Content.Trim().StartsWith($"<@{current_user}>") || msg.Content.Trim().StartsWith($"<@!{current_user}>"))
             {
+                // Send a message with the current server prefix and other help data.
                 await msg.Channel.SendMessageAsync(null, false,
                     new EmbedBuilder()
                     .WithTitle("Clubby")
                     .AddField("The server prefix is:", $"'{Program.config.DiscordBotPrefix}'\nRun `{Program.config.DiscordBotPrefix}help` for more info")
                     .Build());
             }
+            // Check if the user that sent the message is executing a command
             else if (ExecutingCommand.ContainsKey(msg.Author.Id) && ExecutingCommand[msg.Author.Id] != null)
             {
+                // If the command is a cancel call, set the currently executing command to null and inform the user
                 if (msg.Content.StartsWith($"{Program.config.DiscordBotPrefix}cancel"))
                 {
                     await msg.Channel.SendMessageAsync($"{ExecutingCommand[msg.Author.Id].GetCommandHelp().CommandName} command execution has been cancelled!");
                     ExecutingCommand[msg.Author.Id] = null;
                 }
+                // Dispatch to the executing command
                 else
                     await ExecutingCommand[msg.Author.Id].Handle(msg, (msg.Channel as SocketGuildChannel).Guild, this);
             }
+            // If the message is a command it will start with the current prefix
             else if (msg.Content.StartsWith(Program.config.DiscordBotPrefix))
             {
+                // Get the command name from the message
                 string command = ExtractCommand(msg.Content);
+                // Get the type of the command and construct an instance to handle the command.
                 IDiscordCommand cmd = commands.GetInstance(Construct_Identifier(command));
+                // If the command existed cmd will be non null
                 if (cmd != null)
                 {
+                    // Get the minimum permission required to run this command
                     int perms = (int)cmd.GetMinimumPerms();
 
+                    // Get the permission that the user holds
                     int user_perms = Program.config.DiscordPermissions.GetResolvedPerms(msg.Author, msg.IsFromOwner());
+
+                    // Check if the user can use the command
                     if (user_perms >= perms)
                     {
+                        // Sprinkle in a small chance of rebellion
                         if (rand.NextDouble() > 0.999)
                         {
-                            await msg.Channel.SendMessageAsync("no");
+                            await msg.Channel.SendMessageAsync(null,false,new EmbedBuilder().WithTitle("No").WithDescription("no").WithColor(Color.Red).Build());
                             return;
                         }
+
+                        // If the bot is cooperative execute the command's local handler.
                         try
                         {
                             await cmd.Handle(msg, (msg.Channel as SocketGuildChannel).Guild, this);                            
                         }
                         catch(Exception e)
                         {
+                            // If the command fails notify the user as to why.
                             await msg.Channel.SendError(e.Message);
                         }
                     }
+                    // Reject the execution attempt if the user doesn't have the expected permissions
                     else
                         await msg.Channel.SendMessageAsync("You don't have permissions to use this command!");
                 }
                 else
                 {
+                    // Couldn't find the command so let them know.
                     await msg.Channel.SendError($"Couldn't find command: `{Program.config.DiscordBotPrefix}{command}`");
                 }
             }
         }
 
+        /// <summary>
+        /// Constructs a closure to use to find the command required from the plugin manager.
+        /// </summary>
+        /// <param name="cmd">The name of the command</param>
         private Func<Type, bool> Construct_Identifier(string cmd)
         {
             return (t) =>
@@ -125,6 +190,10 @@ namespace Clubby.Discord.CommandHandling
             };
         }
 
+        /// <summary>
+        /// Extracts the command name from a message. 
+        /// </summary>
+        /// <param name="msg">The entire message to extract the command from</param>
         private string ExtractCommand(string msg)
         {
             if (msg.StartsWith(Program.config.DiscordBotPrefix))
